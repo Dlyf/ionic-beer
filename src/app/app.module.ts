b356d8e45bdddcdbcc443ef3406df631aedd8690
Dlyf <domilung@gmail.com>
2019-01-04 09:27:43 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListPage } from './list/list.page';
+import { AddPage } from './add/add.page';
+import { HomePage } from './home/home.page';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [AppComponent, HomePage, ListPage, AddPage ],
+  entryComponents: [AppComponent, HomePage, ListPage, AddPage],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    Observable,
   ],
   providers: [
     StatusBar,
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -30,6 +30,7 @@ export class ListPage implements OnInit {
     }
   }
 
+  
   ngOnInit() {
   }
   // add back when alpha.4 is out
